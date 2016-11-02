aa27626c3bf51d3451202e98de8ea8d9fb8669b5
SkyAo <csvwolf@qq.com>
2016-11-02 10:32:20 +0800

    add lovedController

--- controllers/lovedController.js
+++ controllers/lovedController.js
@@ -1,25 +1,25 @@
 /**
  * Created by SkyAo on 16/5/4.
  *
- * 单身狗计时器
+ * 恋爱计时器：单身狗计时器魔改版
  *
  * 说明：
- * 计算单身持续多少天。
+ * 不解释
  */
 
 module.exports = function(appConfig, appKeys) {
     var moment = appConfig.moment,
-        birthDay = moment([1994, 10, 18]),  // 生日设置，其中月从0开始算，其余不需要更改
+        memorialDay = moment([2016, 9, 25]),  // 生日设置，其中月从0开始算，其余不需要更改
         today = moment(new Date()),
-        duration = moment.duration(today.diff(birthDay)),
+        duration = moment.duration(today.diff(memorialDay)),
         sinceThen = {
             years: duration.get('years'),
             months: duration.get('months'),
             days: duration.get('days'),
-            totalDay: today.diff(birthDay, 'days')
+            totalDay: today.diff(memorialDay, 'days')
         },
-        message = '#小天单身狗计数器# 小天连续单身已经' + sinceThen.totalDay + '天啦！ 也叫'
-                    + sinceThen.years + '年' + sinceThen.months + '月' + sinceThen.days + '日。 关爱小天，从给支付宝打钱做起。',
+        message = '#关爱小天成长协会# 噗通，小天和某人（此处打码）已向世界连续捐助' + sinceThen.totalDay + '天狗粮！ 也叫'
+                    + sinceThen.years + '年' + sinceThen.months + '月' + sinceThen.days + '日。',
         params = {
             access_token: appKeys.developerAccessToken,
             status: message
@@ -37,8 +37,8 @@ module.exports = function(appConfig, appKeys) {
         if (err) {
             appConfig.logger.error(err);
         } else {
-            console.log('本日关爱单身狗成功');
-            appConfig.logger.info('本日关爱单身狗成功');
+            console.log('本日秀恩爱成功');
+            appConfig.logger.info('本日秀恩爱成功');
         }
     };
 
